19be36b5d84eff0eafbe5a51de5f46fcd0590186
James Harris <james.harris@icecave.com.au>
2016-09-06 12:06:49 +1000

    Add test for `AggregateLocator`.

---
+++ src/backend/aggregate_locator_test.go
@@ -0,0 +1,56 @@
+package backend_test
+
+import (
+	"context"
+
+	"github.com/icecave/honeycomb/src/backend"
+	"github.com/icecave/honeycomb/src/name"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("AggregateLocator", func() {
+	var (
+		static1, static2 *backend.StaticLocator
+		subject          backend.AggregateLocator
+	)
+
+	BeforeEach(func() {
+		static1 = &backend.StaticLocator{}
+		static1.Add("foo", &backend.Endpoint{Address: "static1-foo:443"})
+
+		static2 = &backend.StaticLocator{}
+		static2.Add("foo", &backend.Endpoint{Address: "static2-foo:443"})
+		static2.Add("bar", &backend.Endpoint{Address: "static2-bar:443"})
+
+		subject = backend.AggregateLocator{static1, static2}
+	})
+
+	Describe("Locate", func() {
+		It("locates endpoints from the inner locators", func() {
+			endpoint := subject.Locate(
+				context.Background(),
+				name.NormalizeServerName("bar"),
+			)
+			Expect(endpoint).ShouldNot(BeNil())
+			Expect(endpoint.Address).To(Equal("static2-bar:443"))
+		})
+
+		It("searches the inner locators in order", func() {
+			endpoint := subject.Locate(
+				context.Background(),
+				name.NormalizeServerName("foo"),
+			)
+			Expect(endpoint).ShouldNot(BeNil())
+			Expect(endpoint.Address).To(Equal("static1-foo:443"))
+		})
+
+		It("returns nil if none of the inner locators can locate the endpoint", func() {
+			endpoint := subject.Locate(
+				context.Background(),
+				name.NormalizeServerName("unknown"),
+			)
+			Expect(endpoint).To(BeNil())
+		})
+	})
+})
