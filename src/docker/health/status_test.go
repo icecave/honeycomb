39c2192f999a50d8006265de5c7f8e096aba59de
James Harris <james.harris@icecave.com.au>
2016-09-12 16:38:23 +1000

    Add test for `health.Status`.

---
+++ src/docker/health/status_test.go
@@ -0,0 +1,21 @@
+package health_test
+
+import (
+	"github.com/icecave/honeycomb/src/docker/health"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Status", func() {
+	Describe("String", func() {
+		It("indicates that the status check passed", func() {
+			subject := health.Status{true, "<message>."}
+			Expect(subject.String()).To(Equal("Health-check passed: <message>."))
+		})
+
+		It("indicates that the status check failed", func() {
+			subject := health.Status{false, "<message>."}
+			Expect(subject.String()).To(Equal("Health-check failed: <message>."))
+		})
+	})
+})
